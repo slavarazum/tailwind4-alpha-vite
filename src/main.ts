d67066882417056a52780c21d18d795089dd1de5
Slava Razum <razum@qruto.to>
2024-10-24 17:58:12 +0200

    A little makeup 💄 ;)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
+import tailwindLogo from './tailwind.svg'
+import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://vite.dev" class="inline-block" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href="https://tailwindcss.com/" class="inline-block" target="_blank">
+      <img src="${tailwindLogo}" class="logo tailwind" alt="Tailwind logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" class="inline-block" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + Tailwind + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
